5aadcabcac8e0ea5c9c5cbd32f66114a107074ed
Rodrigo Lopes <rodrigo.lopes@corp.globo.com>
2011-01-25 16:07:19 -0200

    Removing unnecessary require of try from activesupport

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,8 @@
+0.1.2
+-----
+
+* Removing unnecessary require of try from activesupport
+
 0.1.1
 -----
 
--- lib/smartgen/engines/base.rb
+++ lib/smartgen/engines/base.rb
@@ -1,5 +1,4 @@
 require 'active_support/core_ext/class/inheritable_attributes'
-require 'active_support/core_ext/try'
 
 module Smartgen
   module Engine
