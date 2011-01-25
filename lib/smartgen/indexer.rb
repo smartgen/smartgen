440f3bafa1de8502d677210b87d7f9b50f647763
Vicente Mundim <vicente.mundim@gmail.com>
2011-01-25 20:48:41 -0200

    Adding support for a table of contents of every generated page

--- lib/smartgen/indexer.rb
+++ lib/smartgen/indexer.rb
@@ -2,25 +2,80 @@ require 'nokogiri'
 
 module Smartgen
   class Indexer
+    attr_accessor :index, :result
+    
     def initialize(contents, options={})
       @contents = contents
-      @result = contents.dup
+      @result = @contents.dup
       @options = options.is_a?(Hash) ? options : {}
+      @index = []
+      
+      parse
     end
     
-    def result
-      html_doc = Nokogiri::HTML(@result)
+    private
+      def parse
+        html_doc = Nokogiri::HTML(@result)
+        tags = html_doc.xpath('//h1|//h2|//h3|//h4|//h5|//h6')
+        
+        current_level = 0
+        parent = nil
+        tags.each do |h_tag|
+          replace_h_tag(h_tag)
+          tag_level = tag_level_for(h_tag)
+
+          if tag_level < current_level
+            parent = parent_with_level(tag_level, parent)
+          elsif tag_level == current_level
+            parent = parent[:parent]
+          end
+          
+          parent = add_to(parent, h_tag, tag_level)
+          current_level = tag_level
+        end
+        
+        remove_parents_from(index)
+      end
       
-      html_doc.css('h1, h2, h3, h4, h5, h6').each do |h_tag|
+      def tag_level_for(h_tag)
+        h_tag.name.sub('h', '').to_i
+      end
+      
+      def add_to(parent, h_tag, tag_level)
+        index_hash(parent, h_tag, tag_level).tap do |child|
+          if parent.nil? # add to index
+            index << child
+          else
+            parent[:children] << child
+          end
+        end
+      end
+      
+      def index_hash(parent, h_tag, tag_level)
+        { :text => h_tag.inner_html, :id => h_tag['id'], :level => tag_level, :parent => parent, :children => [] }
+      end
+      
+      def parent_with_level(level, current_parent)
+        while current_parent.present? && current_parent[:level] >= level
+          current_parent = current_parent[:parent]
+        end
+        
+        current_parent
+      end
+      
+      def remove_parents_from(items)
+        items.each do |item|
+          item.delete(:parent)
+          remove_parents_from(item[:children])
+        end
+      end
+    
+      def replace_h_tag(h_tag)
         snippet = h_tag.to_html
         h_tag['id'] ||= title_to_idx(h_tag.text)
         @result.sub!(snippet, h_tag.to_html)
       end
-      
-      @result
-    end
     
-    private
       def title_to_idx(title)
         title.strip.downcase.gsub(/\s+|_/, '-').delete('^a-z0-9-')
       end
--- spec/lib/smartgen/indexer_spec.rb
+++ spec/lib/smartgen/indexer_spec.rb
@@ -14,7 +14,14 @@ describe Smartgen::Indexer do
   end
   
   def html
-    return <<-HTML
+    "<h1>Some header</h1>"
+  end
+  
+  subject { Smartgen::Indexer.new html }
+
+  describe "addition of IDs" do
+    def html
+      return <<-HTML
 <html>
 <body>
   <h1>A h1 header</h1>
@@ -25,14 +32,53 @@ describe Smartgen::Indexer do
   <h6>A h6 header</h6>
 </body>
 </html>
-HTML
+  HTML
+    end
+  
+    1.upto(6).each do |header_level|
+      it "should add IDs for each <h#{header_level}> tag in the result" do
+        subject.result.should have_tag("h#{header_level}", :id => "a-h#{header_level}-header")
+      end
+    end
   end
   
-  subject { Smartgen::Indexer.new html }
+  describe "index" do
+    def html
+      return <<-HTML
+<html>
+<body>
+  <h1>A h1 header</h1>
+  <h2>A h2 header</h2>
+  <h3>A h3 header</h3>
+  <h3>Some other h3 header</h3>
+  <h2>Another h2 header</h2>
+  <h5>A h5 header</h5>
+  <h2>Yet Another h2 header</h2>
+  <h3>Yet Another h3 header</h3>
+  <h1>Other h1 header</h1>
+</body>
+</html>
+  HTML
+    end
+    
+    it "should return an index with headers data hierarquically distributed" do
+      expected_index = [
+        { :text => 'A h1 header', :id => 'a-h1-header', :level => 1, :children => [
+          { :text => 'A h2 header', :id => 'a-h2-header', :level => 2, :children => [
+            { :text => 'A h3 header', :id => 'a-h3-header', :level => 3, :children => [] },
+            { :text => 'Some other h3 header', :id => 'some-other-h3-header', :level => 3, :children => [] }
+          ] },
+          { :text => 'Another h2 header', :id => 'another-h2-header', :level => 2, :children => [
+            { :text => 'A h5 header', :id => 'a-h5-header', :level => 5, :children => [] }
+          ] },
+          { :text => 'Yet Another h2 header', :id => 'yet-another-h2-header', :level => 2, :children => [
+            { :text => 'Yet Another h3 header', :id => 'yet-another-h3-header', :level => 3, :children => [] }
+          ] },
+        ]},
+        { :text => 'Other h1 header', :id => 'other-h1-header', :level => 1, :children => [] }
+      ]
 
-  1.upto(6).each do |header_level|
-    it "should add IDs for each <h#{header_level}> tag in the result" do
-      subject.result.should have_tag("h#{header_level}", :id => "a-h#{header_level}-header")
+      subject.index.should == expected_index
     end
   end
 end
