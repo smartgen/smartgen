bedac624b0357c6147ad71df63876daf0df8a5c0
Vicente Mundim <vicente.mundim@gmail.com>
2011-02-12 13:37:30 -0200

    Allowing one to pass a different resource name for rake task

--- lib/smartgen/rake_task.rb
+++ lib/smartgen/rake_task.rb
@@ -20,15 +20,31 @@ module Smartgen
   #
   # It yields a Configuration, in fact it calls Resource#configure, so you
   # can use any of the configurations here.
+  #
+  # You may also use an existing configuration, while still having a different
+  # task name:
+  #   Smartgen[:my_config] do |config|
+  #     config.layout = 'layout.html.erb'
+  #   end
+  #
+  #   Smartgen::RakeTask.new :watcher, :my_config do |config|
+  #     config.src_files = ['doc/**/*']
+  #     config.output_folder = 'public/doc'
+  #   end
+  #
+  # This will use Smartgen[:generate_doc] config, but you will call it with:
+  #
+  #   rake watcher
   class RakeTask < ::Rake::TaskLib
-    def initialize(name=nil, &block)
-      name = name || :smartgen
+    def initialize(resource_or_task_name=nil, resource_name=nil, &block)
+      task_name = resource_or_task_name || :smartgen
+      resource_name = resource_name || task_name
       
-      Smartgen[name].configure(&block)
+      Smartgen[resource_name].configure(&block)
       
       desc("Generate smartgen files") unless ::Rake.application.last_comment
-      task name do
-        Smartgen[name].generate!
+      task task_name do
+        Smartgen[resource_name].generate!
       end
     end
   end
